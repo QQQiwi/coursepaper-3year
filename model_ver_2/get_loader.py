450362bc322980dee3623a898fafd4962068bd5c
QQQiwi <ulitin.ivan3004@mail.ru>
2022-04-04 17:36:15 +0400

    Определение классов для объектов загрузки данных для модели

--- model_ver_2/get_loader.py
+++ model_ver_2/get_loader.py
@@ -7,23 +7,55 @@ from torch.utils.data import DataLoader, Dataset
 from PIL import Image
 import torchvision.transforms as transforms
 
-spacy_eng = spacy.load('en_core_web_sm')
+# будут определяться слова английского языка
+SPACY_ENG = spacy.load('en_core_web_sm')
 
 
 class Vocabulary:
+    """
+        Класс словаря, описывающий структуры, содержащие слова, которые запоминает модель в процессе обучения и на
+        основе которых формирует подпись к изображению.
+    """
+
     def __init__(self, freq_threshold):
+        """
+            Функция инициализации, при котором генерируются объекты типа словарь (первый объект - определяет слово по
+            индексу, второй - определяет индекс по слову).
+
+        :param freq_threshold: максимальное количество повторяющихся слов
+        """
+
         self.itos = {0: "<PAD>", 1: "<SOS>", 2: "<EOS>", 3: "<UNK>"}
         self.stoi = {"<PAD>": 0, "<SOS>": 1, "<EOS>": 2, "<UNK>": 3}
         self.freq_threshold = freq_threshold
 
     def __len__(self):
+        """
+            Функция возвращает длину словаря.
+
+        :return: длина словаря (ключ - индекс, значение - слово в словаре)
+        """
         return len(self.itos)
 
     @staticmethod
     def tokenizer_eng(text):
-        return [tok.text.lower() for tok in spacy_eng.tokenizer(text)]
+        """
+            Функция токенизирует слова некоторого текста.
+
+        :param text: входной текст на английском языке
+        :return: список токенов для каждого слова в тексте
+        """
+
+        # пример: "I love bananas" -> ["i", "love", "bananas"]
+        return [tok.text.lower() for tok in SPACY_ENG.tokenizer(text)]
 
     def build_vocabulary(self, sentence_list):
+        """
+            Функция осуществляет заполнение словаря (добавление слов в него и определение для каждого из них индекса).
+
+        :param sentence_list: список предложений, являющихся подписям к изображениям
+        """
+
         frequencies = {}
         idx = 4
 
@@ -40,6 +72,12 @@ class Vocabulary:
                     idx += 1
 
     def numericalize(self, text):
+        """
+            Функция заменяет слова токенизированного текста соответствующими этим словам индексами.
+
+        :param text: некоторый текст, подлежащий токенизированию
+        :return: список индексов
+        """
         tokenized_text = self.tokenizer_eng(text)
 
         return [
@@ -49,7 +87,20 @@ class Vocabulary:
 
 
 class FlickrDataset(Dataset):
+    """
+        Определение класса датасета для набора данных Flickr.
+    """
+
     def __init__(self, root_dir, caption_file, transform=None, freq_threshold=5):
+        """
+            Инициализирует объект датасета, определяя списки путей к изображениям и соответствующим им подписям, а также
+            формирует словарь.
+
+        :param root_dir: корневая директория, откуда будут браться изображения
+        :param caption_file: адрес файла с подписями к изображениям
+        :param transform: некоторый объект, преобразовывающий изображения заданным образом
+        :param freq_threshold: максимальное количество повторяющихся слов
+        """
         self.root_dir = root_dir
         self.df = pd.read_csv(caption_file)
         self.transform = transform
@@ -61,9 +112,21 @@ class FlickrDataset(Dataset):
         self.vocabulary.build_vocabulary(self.captions.tolist())
 
     def __len__(self):
+        """
+            Функция возвращает длину датафрейма (т.е. количество различных подписей к изображениям).
+
+        :return: длина датафрейма
+        """
         return len(self.df)
 
     def __getitem__(self, index):
+        """
+            Функция определяет возможность взятия одной сущности из датасета (в частности, изображения и соответствующей
+            ему подписи).
+
+        :param index: индекс сущности, которую необходимо считать
+        :return: кортеж из трансформированного изображения и тензора преобразованной в индексы слов подписи
+        """
         caption = self.captions[index]
         img_id = self.imgs[index]
         img = Image.open(os.path.join(self.root_dir, img_id)).convert("RGB")
@@ -79,6 +142,10 @@ class FlickrDataset(Dataset):
 
 
 class MyCollate:
+    """
+        Класс MyCollate для помещения в batch значений датасета.
+
+    """
     def __init__(self, pad_idx):
         self.pad_idx = pad_idx
 
@@ -100,6 +167,19 @@ def get_loader(
         shuffle=True,
         pin_memory=True
 ):
+    """
+    Функция get_loader осуществляет загрузку данных для работы модели машинного обучения с этими данными.
+
+    :param root_folder: корневая директория, откуда будут браться изображения
+    :param annotation_file: путь к файлу с подписями для изображений
+    :param transform: преобразование, которое необходимо сделать с изображениями
+    :param batch_size: размер batch
+    :param num_workers: определяет количество некоторого ресурса компьютера для ускорения работы
+    :param shuffle: параметр перемешивания сущностей
+    :param pin_memory: определяет количество некоторого ресурса компьютера
+
+    :return: кортеж из объекта загрузки данных и объекта датасета
+    """
     dataset = FlickrDataset(root_folder, annotation_file, transform)
     pad_idx = dataset.vocabulary.stoi["<PAD>"]
 
@@ -116,6 +196,9 @@ def get_loader(
 
 
 def main():
+    """
+        Запуск функцию осуществляет проверку корректности созданных классов.
+    """
     transform = transforms.Compose(
         [
             transforms.Resize((224, 224)),
